903534072084db61137504f80dd5ef21e69a0d17
Denys Krasnokutskyi <vareted@gmail.com>
2024-03-08 21:54:29 +0200

    add: sort

--- src/Config/system.php
+++ src/Config/system.php
@@ -48,7 +48,29 @@ return [
                 'validation'    => 'required',
                 'channel_based' => false,
                 'locale_based'  => true,
-            ],
+            ], [
+                'name' => 'sort',
+                'title' => 'monobank::app.admin.system.sort',
+                'type' => 'select',
+                'options' => [
+                    [
+                        'title' => '1',
+                        'value' => 1
+                    ], [
+                        'title' => '2',
+                        'value' => 2
+                    ], [
+                        'title' => '3',
+                        'value' => 3
+                    ], [
+                        'title' => '4',
+                        'value' => 4
+                    ], [
+                        'title' => '5',
+                        'value' => 5
+                    ]
+                ],
+            ]
         ]
     ]
 ];
